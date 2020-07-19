d468653df95a0e6288f668fc8f54bf54ede256f2
Wayne Van Son <waynevanson@gmail.com>
2020-07-19 15:31:21 +1000

    tests: refactor set tests

--- src/normalize.test.ts
+++ src/normalize.test.ts
@@ -89,8 +89,85 @@ describe(normalize, () => {
       expect(postsGet).toStrictEqual(postsExpected);
     });
 
+    describe("set", () => {
+      const state = { users: [], posts: [] };
+
+      test("users", () => {
+        const usersSet = pipe(
+          normalized,
+          E.map((optional) => optional.users.set([{ id: "2" }])),
+          E.ap(E.right(state))
+        );
+
+        const usersExpected = E.right({
+          users: [{ id: "2" }],
+          posts: [],
+        });
+        expect(usersSet).toStrictEqual(usersExpected);
+      });
+
+      test("posts", () => {
+        const postsSet = pipe(
+          normalized,
+          E.map((optional) => optional.posts.set([{ id: "5" }])),
+          E.ap(E.right(state))
+        );
+
+        const postsExpected = E.right({
+          posts: [{ id: "5" }],
+          users: [],
+        });
+        expect(postsSet).toStrictEqual(postsExpected);
+      });
+    });
+  });
+
+  describe.skip("Two entities, one relationships", () => {
+    type User = {
+      id: string;
+    };
+
+    type Post = {
+      id: string;
+      author: User;
+    };
+
+    const users = () => makeEntity<User>()();
+    const posts = () => makeEntity<Post>()({ author: users });
+    const schema = { users, posts };
+    const normalized = normalize(schema);
+
+    test("Normalized object shape", () => {
+      const expected = E.right({
+        users: expect.any(Optional),
+        posts: expect.any(Optional),
+      });
+
+      expect(normalized).toStrictEqual(expected);
+    });
+
+    test("getOption for both", () => {
+      const state = { users: [{ id: "2" }], posts: [{ id: "5", author: "2" }] };
+
+      const usersGet = pipe(
+        normalized,
+        E.map((optional) => optional.users.getOption(state))
+      );
+
+      const userExpected = E.right(O.some([{ id: "2" }]));
+      expect(usersGet).toStrictEqual(userExpected);
+
+      const postsGet = pipe(
+        normalized,
+        E.map((optional) => optional.posts.getOption(state))
+      );
+
+      const postsExpected = E.right(O.some([{ id: "5", author: { id: "2" } }]));
+      expect(postsGet).toStrictEqual(postsExpected);
+    });
+
     test("set", () => {
-      const state = { users: [], posts: [{ id: "4" }] };
+      const state = { users: [], posts: [{ id: "4", author: "3" }] };
 
       const usersSet = pipe(
         normalized,
